1bf39654ca5c62bdeb421a0070c1d018e2d61729
HaydenWalton12 <70973137+HaydenWalton12@users.noreply.github.com>
2022-10-03 10:58:32 +0100

    Update Shader.h

--- FrameworkDX11/Shader.h
+++ FrameworkDX11/Shader.h
@@ -2,27 +2,39 @@
 
 #include <wrl.h>
 #include <d3d11.h>
-
+#include <d3dcompiler.h>
 class Shader
 {
+public:
+
+	//Constructor Overloading To Either Create Vertex/Pixel Shader
+	Shader();
+	//Create Vertex Shader
+	Shader(ID3D11Device* device, ID3D11DeviceContext* device_context,  WCHAR* vertex_shader_path, ID3D11InputLayout* vertex_layout);
+	//Create Pixel Shader
+	Shader(ID3D11Device* device, WCHAR* pixel_shader_path);
+
+	Microsoft::WRL::ComPtr<ID3D11VertexShader> GetVertexShader() { return _VertexShader; };
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> GetPixelShader() { return _PixelShader; };
+	//ID3D11InputLayout - Describes an element expressed for the input assembler stage (this stage determines what data will be entered for later processing within rendering/dx11 pipeline).
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> GetVertexLayout() { return _VertexLayout; };
+
 private:
 	//ComPtr - Creates a smart pointer that creates an interface to the type given , gives further functionality of raw pointer managament via this function
 	//Smart Pointer - Essentially wraps around a raw/primitive pointer , manages the lifetime of this pointer automatically.
 	//E.G Raw Pointers are stupid, you ave to manually destroy the object when no longer used, can become tedious in code.
+
+	//Smart Pointers Contain two pointers, the raw pointer to manage the object referenced in memory , and the "smart pointer" which manages this pointer itself.
+
 	
+
 	Microsoft::WRL::ComPtr<ID3D11VertexShader> _VertexShader;
 	Microsoft::WRL::ComPtr<ID3D11PixelShader> _PixelShader;
 	Microsoft::WRL::ComPtr<ID3D11InputLayout> _VertexLayout;
+	WCHAR* _File;
 
 	HRESULT CompileShaderFromFile(const WCHAR* fileName, LPCSTR entryPoint, LPCSTR shaderModel, ID3DBlob** blobOut);
-public:
 
-	Shader(ID3D11Device* device, WCHAR* vertex_shader_path  , D3D11_INPUT_ELEMENT_DESC* vertex_layout)
-
-	Microsoft::WRL::ComPtr<ID3D11VertexShader> GetVertexShader() { return _VertexShader; };
-	Microsoft::WRL::ComPtr<ID3D11PixelShader> GetPixelShader() { return _PixelShader; };
-
-	//ID3D11InputLayout - Describes an element expressed for the input assembler stage (this stage determines what data will be entered for later processing within rendering/dx11 pipeline).
-	Microsoft::WRL::ComPtr<ID3D11InputLayout> GetVertexLayout() { return _VertexLayout; };
+	HRESULT hr;
 };
 
